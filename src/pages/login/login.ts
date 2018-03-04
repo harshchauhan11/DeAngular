2f21c5a1530ec55a8c4d1e77b7855495b4f29a2d
Harsh11 <harsh.h.chauhan@gmail.com>
2018-03-05 00:42:09 +0530

    Delvrt Angular added.

---
+++ src/pages/login/login.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { TabsPage } from '../tabs/tabs';
+
+/**
+ * Generated class for the LoginPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html',
+})
+export class Login {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad Login');
+  }
+
+  login(){
+    // Your app login API web service call triggers 
+    this.navCtrl.push(TabsPage, {}, {animate: false});
+  }
+
+}

6d898610bcf069ff90719243cb8b9ddea5ee6485
Harsh Chauhan <harsh.h.chauhan@gmail.com>
2018-04-30 02:09:15 +0530

    Login and Signup APIs implemented.

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,25 +1,48 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the SignupPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { Component } from "@angular/core";
+import { IonicPage, NavController, NavParams } from "ionic-angular";
+import { TabsPage } from "../tabs/tabs";
+import { Login } from "../login/login";
+import { AuthServiceProvider } from "../../providers/auth-service/auth-service";
 
 @IonicPage()
 @Component({
-  selector: 'page-signup',
-  templateUrl: 'signup.html',
+  selector: "page-signup",
+  templateUrl: "signup.html"
 })
 export class Signup {
+  responseData: any;
+  userData = { username: "", password: "", name: "", email: "" };
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public authService: AuthServiceProvider
+  ) {}
+
+  signup() {
+    this.authService.postData(this.userData, "signup").then(
+      result => {
+        this.responseData = result;
+        if (this.responseData.userData) {
+          console.log(this.responseData);
+          localStorage.setItem("userData", JSON.stringify(this.responseData));
+          this.navCtrl.push(TabsPage);
+        } else {
+          console.log("User already exists");
+        }
+      },
+      err => {
+        // Error log
+      }
+    );
+  }
+
+  login() {
+    //Login page link
+    this.navCtrl.push(Login);
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad Signup');
+    console.log("ionViewDidLoad Signup");
   }
-
 }
